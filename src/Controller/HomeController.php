fa9f9d48859d72cda87334e9bacdbf2a46efdf3c
Ken <kenserikawalucio@gmail.com>
2020-05-13 16:24:30 +0200

    Fix empty space

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,9 +7,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/home", name="home")
-     */
     public function index()
     {
         return $this->render('home/index.html.twig', [

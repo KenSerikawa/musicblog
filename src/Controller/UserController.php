c8aa02cdf58fd31a7a5df4cd01db55bba5b0b8fc
Ken <kenserikawalucio@gmail.com>
2020-05-15 23:07:37 +0200

    Add registration/create method

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
     public function index()
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $newUser = new User();
+        $newUser->setUuid();
+        
+        $form = $this->createForm(UserType::class, $newUser);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login') ;
+        }
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

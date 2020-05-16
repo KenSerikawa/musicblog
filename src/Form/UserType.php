51fab478613337bb0d1a7e344313e025da91bf87
Ken <kenserikawalucio@gmail.com>
2020-05-16 19:05:57 +0200

    Add input file type (profile image)

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -28,6 +29,12 @@ class UserType extends AbstractType
         ])
         ->add('email', EmailType::class, [
             'required' => true
+        ])
+        ->add('profile_image', FileType::class, [
+            'required' => false,
+            "label" => "Imagen:",
+            "attr" =>array("class" => "form-control"),
+            "data_class" => null
         ])
         ->add('register', SubmitType::class)
         ;
